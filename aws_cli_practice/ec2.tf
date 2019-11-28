6a951ce69a2bc5ae44a74bd2d800ba0acbf2172e
atsushi-815 <atsushi586@yahoo.co.jp>
2019-11-28 10:35:47 +0900

    作成

---
+++ aws_cli_practice/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "sandbox" {
+  count         = 1
+  ami           = "ami-785c491f" # Ubuntu 16.04 LTS official ami
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "${format("kyariko-%02d", count.index + 1)}"
+  }
+}
