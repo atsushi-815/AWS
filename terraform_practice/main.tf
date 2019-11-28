931fc7f3b8aa889cd17c5515239df68684db8dff
atsushi-815 <atsushi586@yahoo.co.jp>
2019-11-28 10:59:21 +0900

    apacheインストール

--- terraform_practice/main.tf
+++ terraform_practice/main.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "example" {
-   ami           = "ami-0f9ae750e8274075b"
-   instance_type = "t3.micro"
-
-   tags = {
-      Name = "example"
-   }
+  ami           = "ami-0f9ae750e8274075b"
+  instance_type = "t3.micro"
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
 }
