8d19ad39147e3a98c5c90f9b5fd6645e9e639a69
atsushi-815 <atsushi586@yahoo.co.jp>
2019-11-30 14:20:04 +0900

    修正

--- terraform_practice/main.tf
+++ terraform_practice/main.tf
@@ -1,8 +1,17 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0f9ae750e8274075b"
-  instance_type = "t3.micro"
-}
+resource "aws_security_group" "example_ec2" {
+    name = "example-ec2"
 
-output "example_instance_id" {
-  value = aws_instance.example.id
+    ingress {
+        from_port    = 80
+        to_port      = 80
+        protocol     = "tcp"
+        cidr_blocks  = ["0.0.0.0/0"]
+    }
+
+    egress {
+      from_port    = 0
+      to_port      = 0
+      protocol     = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
 }
